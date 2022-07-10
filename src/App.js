ec40da209aabcf376dd173f386c144e31bc7b9ef
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-07-10 20:01:50 -0300

    as
    as

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Title } from "./component/title";
+import SearchFrom from "./component/SearchForm";
+import { MoviesList } from "./component/MoviesList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "bulma/css/bulma.css";
+
+class App extends Component {
+  state = { usedSearch: false, results: [] };
+
+  _handleResults = (results) => {
+    this.setState({ results, usedSearch: true });
+  };
+  _renderResults() {
+    return this.state.results.length === 0 ? (
+      <p>Sorry! Results not found...</p>
+    ) : (
+      <MoviesList movies={this.state.results} />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <div className="SearchForm-wrapper">
+          <SearchFrom onResults={this._handleResults} />
+        </div>
+        {this.state.usedSearch ? (
+          this._renderResults()
+        ) : (
+          <small>Use the form search a movie c:</small>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/title.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Title = ({ children }) => <h1 class="title">{children}</h1>;
